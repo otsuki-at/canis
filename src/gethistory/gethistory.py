692bb5e1020fc37b50d1e04a385b9bf7d42a5f04
otsuki-at <otsuki2024@s.okayama-u.ac.jp>
2025-03-14 11:20:36 +0900

    Update confirmation waiting time to 0 sec

--- src/gethistory/gethistory.py
+++ src/gethistory/gethistory.py
@@ -4,6 +4,7 @@ import signal
 from lark import Lark
 from lark import Transformer
 import time
+from datetime import datetime
 import pathlib
 import redis
 from dotenv import load_dotenv
@@ -20,25 +21,39 @@ class LogFile:
         self.filename = logfile_name
         self.f = pathlib.Path(self.filename)
         self.lastdate = None
-        self.fd = open(self.filename)
+        self.fd = open(self.filename, "r")
+        self.last_position = 0  # 前回の読み取り位置を追跡
 
     def update_exists(self):
         lastmdate = self.f.stat().st_mtime
-
         if self.lastdate != lastmdate:
             self.lastdate = lastmdate
             return True
-        else:
-            return False
+        return False
 
     def get_update(self):
-        while(True):
+        while True:
             if self.update_exists():
-                logs = self.fd.read().splitlines()
+                self.fd.seek(self.last_position)  # 前回の位置から読み取る
+                new_data = self.fd.read()  # 新たに追記された部分を全て取得
+                if not new_data:
+                    # time.sleep(4)
+                    continue
 
-                return logs
-            else:
-                time.sleep(4)
+                logs = new_data.splitlines(keepends=True)
+
+                # 最後の行が不完全なら、その行を取り除いて次回処理する準備
+                if logs and not logs[-1].endswith("\n"):
+                    incomplete_line = logs.pop(-1)  # 不完全な行を取り除く
+                    self.last_position = self.fd.tell() - len(incomplete_line)
+
+                else:
+                    # 完全な行が処理された場合は、位置を更新
+                    self.last_position = self.fd.tell()
+                # 完全な行だけを返す
+                return [line.strip() for line in logs]
+            # else:
+            #     time.sleep(4)
 
     def __del__(self):
         self.fd.close()
@@ -78,8 +93,13 @@ def main():
         logs = logfile.get_update()
         for log in logs:
             publisher.publish(log)
+            # t1_end = datetime.now().isoformat()
+            # with open (logdir + '/t1.log', 'a') as f:
+            #     print(t1_end,log, end="\n", file=f)
             with open(logdir + '/history.log', 'a') as f:
-                print(log, file=f)
+                print(log,end='\n',file=f)
+        # with open(logdir + '/history.log', 'a') as f:
+            # print(logs,end='\n',file=f)
 
 if __name__ == "__main__":
     main()
